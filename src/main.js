e00d6ac417fcb12dcdeef5059eae51afbafba3a2
Legolas9609 <StarZgred@interia.pl>
2020-05-02 07:00:56 +0200

    Initial message

--- src/main.js
+++ src/main.js
@@ -4,18 +4,50 @@ import VeeValidate from 'vee-validate'
 import lineClamp from 'vue-line-clamp'
 import ReadMore from 'vue-read-more';
 
-import { MdRipple, MdAvatar, MdButton, MdMenu, MdList } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import './md_default.css'
 
-import {BootstrapVue} from 'bootstrap-vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+    faEllipsisV,
+    faEye,
+    faFilter,
+    faPlay,
+    faSearch,
+    faSortDown,
+    faSortUp,
+    faThumbsDown,
+    faThumbsUp,
+    faTimes,
+    faLock,
+    faLockOpen,
+    faGlobe,
+    faTrashAlt,
+    faWindowClose
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 import App from "./App";
 import {router} from './routes';
 import 'v-autocomplete/dist/v-autocomplete.css'
 
+library.add(faTimes);
+library.add(faPlay);
+library.add(faSearch);
+library.add(faThumbsUp);
+library.add(faThumbsDown);
+library.add(faEye);
+library.add(faSortUp);
+library.add(faSortDown);
+library.add(faFilter);
+library.add(faEllipsisV);
+library.add(faLock);
+library.add(faLockOpen);
+library.add(faGlobe);
+library.add(faTrashAlt);
+library.add(faWindowClose);
+
 Vue.config.productionTip = false;
 
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(VeeValidate, {
     // This is the default
@@ -26,15 +58,9 @@ Vue.use(VeeValidate, {
     errorBagName: 'veeErrors'
 });
 
-Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 Vue.use(ReadMore);
 
-Vue.use(MdRipple);
-Vue.use(MdAvatar);
-Vue.use(MdButton);
-Vue.use(MdMenu);
-Vue.use(MdList);
 
 Vue.use(lineClamp, {
     // plugin options
